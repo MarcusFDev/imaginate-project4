0d93ba8ee20997633ba8fa6fec7b5c620f46473f
Marcus F <marcusf.dev@gmail.com>
2024-08-29 11:48:10 +0000

    Add: Project intialization & Heroku connection.

---
+++ imaginate/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for imaginate project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imaginate.settings')
+
+application = get_asgi_application()
---
+++ imaginate/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for imaginate project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imaginate.settings')
+
+application = get_wsgi_application()
