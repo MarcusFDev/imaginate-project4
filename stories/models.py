c85a3c7c04c2dfcddbf18b528aea08a0313b3517
Marcus F <marcusf.dev@gmail.com>
2024-08-29 18:19:25 +0000

    Add: Comment model & migrated.

--- stories/models.py
+++ stories/models.py
@@ -7,6 +7,9 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Story(models.Model):
+    """
+    Stores a single blog post entry related to :model:`auth.User`.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -25,3 +28,26 @@ class Story(models.Model):
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
+
+
+class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`auth.User`
+    and :model:`imaginate.Story`.
+    """
+    story = models.ForeignKey(
+        Story, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(auto_now=True)
+    upvotes = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
