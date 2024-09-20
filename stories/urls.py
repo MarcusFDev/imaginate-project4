0502a836a4e4eee9191cf53907732575df855a3a
Marcus F <marcusf.dev@gmail.com>
2024-09-20 13:46:23 +0000

    Add: Story page content functionality.

--- stories/urls.py
+++ stories/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.StoryList.as_view(), name='home'),
+    path('<slug:slug>/', views.story_page, name='story_page'),
 ]
--- stories/views.py
+++ stories/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render # noqa
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Story
 
@@ -8,3 +8,27 @@ class StoryList(generic.ListView):
     queryset = Story.objects.filter(status=1, is_private=False)
     template_name = "stories/index.html"
     paginate_by = 15
+
+
+def story_page(request, slug):
+    """
+    Display an individual :model:`stories.Story`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`stories.Story`.
+
+    **Template:**
+
+    :template:`stories/story_page.html`
+    """
+
+    queryset = Story.objects.filter(status=1)
+    story = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "stories/story_page.html",
+        {"story": story},
+    )
