993d57b6965ee4d73ff98f237a4a2a014e6a6f24
Marcus F <marcusf.dev@gmail.com>
2024-09-20 12:11:14 +0000

    Add: Styling for pagination.

--- imaginate/settings.py
+++ imaginate/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 # DEBUG = False
 
 ALLOWED_HOSTS = [
-    "8000-marcusfdev-imaginatepro-1j64ulubk8k.ws-eu116.gitpod.io",
+    "8000-marcusfdev-imaginatepro-nnvyly3p6cc.ws-eu116.gitpod.io",
     ".herokuapp.com",
 ]
 
