bb9983609bf220a2366c9f06b0db8f5ed9316f3c
Marcus F <marcusf.dev@gmail.com>
2024-09-20 19:22:09 +0000

    Add: Static files implemented.

---
+++ staticfiles/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('#nav-home').hover(
+        function() {
+            // Mouse enters the element
+            $(this).find('i').addClass('icon-beat');
+        },
+        function() {
+            // Mouse leaves the element
+            $(this).find('i').removeClass('icon-beat');
+        }
+    );
+    $('#nav-stories').hover(
+        function() {
+            // Mouse enters the element
+            $(this).find('i').addClass('icon-bounce');
+        },
+        function() {
+            // Mouse leaves the element
+            $(this).find('i').removeClass('icon-bounce');
+        }
+    );
+    $('#nav-profile').hover(
+        function() {
+            // Mouse enters the element
+            $(this).find('i').addClass('icon-beat');
+        },
+        function() {
+            // Mouse leaves the element
+            $(this).find('i').removeClass('icon-beat');
+        }
+    );
+});
