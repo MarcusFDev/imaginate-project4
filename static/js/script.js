f049e86a024c0adc8f8db719d4d1700628c593ac
Marcus F <marcusf.dev@gmail.com>
2024-09-21 18:49:33 +0000

    Add: Profile page styling.

--- static/js/script.js
+++ static/js/script.js
@@ -30,3 +30,22 @@ $(document).ready(function(){
         }
     );
 });
+
+$(document).ready(function() {
+    console.log("Loaded jQuery Function");
+
+    $('.btn-close').on('click', function() {
+        console.log("Received Request.");
+        
+        // Add the fade-out animation to #info-alert
+        $('#info-alert').addClass('animate__animated animate__fadeOutUp');
+
+        // Start sliding up the #info-section at the same time
+        $('#info-section').slideUp(400, function() {
+            // Optionally, you can add the 'hidden' class after it's slid up if needed
+            $(this).addClass('hidden');
+        });
+
+        console.log("Request Complete.");
+    });
+});
