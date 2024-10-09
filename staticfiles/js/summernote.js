6a5b97f631bd7835bb7da948ed232ce79cd619a9
Marcus F <marcusf.dev@gmail.com>
2024-10-09 13:43:01 +0000

    Add: Edit About modal with summernote.

--- staticfiles/js/summernote.js
+++ staticfiles/js/summernote.js
@@ -1,6 +1,35 @@
 $(document).ready(function() {
-    // Hide the first note-palette (Background Color)
-    $('.note-palette').first().hide();
+    $('#aboutme_textarea').summernote({
+        toolbar: [
+            ['font', ['bold', 'italic', 'underline', 'clear']],
+            ['ul'],
+            ['ol'],
+            
+        ],
+        colors: [
+            ['#FF0000', '#00FF00', '#0000FF', '#FFFF00'],
+            ['#FFA500', '#800080', '#FFFFFF']
+        ],
+        iframe: false,
+        height: 600,
+        toolbar_BS5: true,
+    });
 });
 
 
+$(document).ready(function() {
+    $('#aboutmemodal').on('shown.bs.modal', function () {
+        if (!$('#aboutme_textarea').hasClass('summernote')) {
+            $('#aboutme_textarea').summernote({
+                focus: true
+            });
+        }
+    });
+  
+    // Optional: Destroy Summernote when modal is hidden (to avoid re-initialization issues)
+    $('#aboutmemodal').on('hidden.bs.modal', function () {
+        if ($('#aboutme_textarea').hasClass('summernote')) {
+            $('#aboutme_textarea').summernote('destroy');
+        }
+    });
+});
