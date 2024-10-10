89ff4b50aaf80351c6292840c117d6d179e19cac
Marcus F <marcusf.dev@gmail.com>
2024-10-10 01:41:05 +0000

    Add: Replaced About with UserProfile model.

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
-# Create your models here.
-class About(models.Model):
-    title = models.CharField(max_length=200)
-    updated_on = models.DateTimeField(auto_now=True)
-    content = models.TextField(max_length=1500)
-    acc_user = models.ForeignKey(User, on_delete=models.CASCADE)
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=1500, blank=True)
+    profile_picture = models.ImageField(
+        default='static/images/default-user-icon.webp')
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    last_login = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return self.title
+        return f"{self.user.username}'s Profile"
