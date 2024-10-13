65231d93b6c4e330bd69186ef41e8ff0178d1061
Marcus F <marcusf.dev@gmail.com>
2024-10-13 23:09:29 +0000

    Add: Summernote styling for Story editor.

--- static/js/summernote.js
+++ static/js/summernote.js
@@ -33,3 +33,41 @@ $(document).ready(function() {
         }
     });
 });
+
+
+$(document).ready(function() {
+    $('#id_excerpt').summernote({
+        toolbar: [
+            ['font', ['bold', 'italic', 'underline', 'clear']],
+            ['ul'],
+            ['ol'],
+            
+        ],
+        colors: [
+            ['#FF0000', '#00FF00', '#0000FF', '#FFFF00'],
+            ['#FFA500', '#800080', '#FFFFFF']
+        ],
+        iframe: false,
+        height: 300,
+        toolbar_BS5: true,
+    });
+});
+
+
+$(document).ready(function() {
+    $('#id_content').summernote({
+        toolbar: [
+            ['font', ['bold', 'italic', 'underline', 'clear']],
+            ['ul'],
+            ['ol'],
+            
+        ],
+        colors: [
+            ['#FF0000', '#00FF00', '#0000FF', '#FFFF00'],
+            ['#FFA500', '#800080', '#FFFFFF']
+        ],
+        iframe: false,
+        height: 1000,
+        toolbar_BS5: true,
+    });
+});
