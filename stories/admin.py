18e1da839c903c193869adb609c7a70a5fa17524
Marcus F <marcusf.dev@gmail.com>
2024-10-16 19:02:40 +0000

    Add: Docstrings added to admin.py functions.

--- stories/admin.py
+++ stories/admin.py
@@ -4,10 +4,30 @@ from django_summernote.admin import SummernoteModelAdmin
 
 
 class UpvoteRangeFilter(admin.SimpleListFilter):
+    """
+    A custom admin filter for filtering Story objects by their upvote range.
+
+    Provides options to filter stories with the following upvote ranges:
+        - 0 to 10 upvotes
+        - 11 to 50 upvotes
+        - 51 to 100 upvotes
+        - 100+ upvotes
+
+    Methods:
+        lookups: Returns the list of upvote range options displayed in the
+        admin.
+        queryset: Filters the queryset based on the selected upvote range.
+    """
     title = 'Upvotes'
     parameter_name = 'upvotes_range'
 
     def lookups(self, request, model_admin):
+        """
+        Defines the lookup options for filtering by upvote ranges.
+
+        Returns:
+            A list of tuples where each tuple represents a filter option.
+        """
         return [
             ('0-10', '0 to 10'),
             ('11-50', '11 to 50'),
@@ -16,6 +36,17 @@ class UpvoteRangeFilter(admin.SimpleListFilter):
         ]
 
     def queryset(self, request, queryset):
+        """
+        Filters the queryset based on the selected upvote range.
+
+        Args:
+            request: The HTTP request object.
+            queryset: The original queryset of stories.
+
+        Returns:
+            The filtered queryset based on the selected upvote range, or
+            the original queryset if no filter is selected.
+        """
         if self.value() == '0-10':
             return queryset.filter(upvotes__range=(0, 10))
         if self.value() == '11-50':
@@ -29,6 +60,21 @@ class UpvoteRangeFilter(admin.SimpleListFilter):
 
 @admin.register(Story)
 class StoryAdmin(SummernoteModelAdmin):
+    """
+    Admin interface customization for the Story model.
+
+    Customizations:
+        - list_display: Specifies the fields displayed in the admin story list
+          view.
+        - search_fields: Adds a search bar that searches stories by title and
+          author's username.
+        - list_filter: Adds filters for story status, creation date, privacy,
+          and upvote range.
+        - prepopulated_fields: Automatically fills in the 'slug' field based
+          on the 'title'.
+        - summernote_fields: Specifies the content field to use the Summernote
+          editor for rich text input.
+    """
 
     list_display = ('title', 'slug', 'author', 'upvotes', 'is_private',
                     'status', 'created_on',)
