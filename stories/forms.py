e53da9200eedd9b7c05a31a3cf3ab11ec1891084
Marcus F <marcusf.dev@gmail.com>
2024-10-16 19:05:23 +0000

    Add: Docstrings to story form functions.

--- stories/forms.py
+++ stories/forms.py
@@ -4,6 +4,13 @@ from django.utils.html import strip_tags
 
 
 class CommentForm(forms.ModelForm):
+    """
+    A form for submitting or editing a comment related to a story.
+
+    Fields:
+        body: TextArea for the comment text, with a placeholder and a
+        character limit of 500.
+    """
     class Meta:
         model = Comment
         fields = ('body',)
@@ -13,6 +20,10 @@ class CommentForm(forms.ModelForm):
         }
 
     def clean(self):
+        """
+        Cleans and validates the 'body' field by checking:
+        - Length: Ensures the comment is no more than 500 characters.
+        """
         cleaned_data = super().clean()
         comment = cleaned_data.get('body')
 
@@ -23,6 +34,17 @@ class CommentForm(forms.ModelForm):
 
 
 class StoryForm(forms.ModelForm):
+    """
+    A form for creating or editing a story with title, excerpt, and content
+    fields.
+
+    Fields:
+        title: TextInput for the story title with a placeholder and character
+        limit.
+        excerpt: TextArea for the story excerpt with a placeholder and
+        character limit.
+        content: TextArea for the main story content with a placeholder.
+    """
 
     class Meta:
         model = Story
@@ -43,6 +65,11 @@ class StoryForm(forms.ModelForm):
         }
 
     def clean_title(self):
+        """
+        Validates the 'title' field by ensuring:
+        - The title is not empty.
+        - The title does not exceed 50 characters.
+        """
         title = self.cleaned_data.get('title')
         if not title or title.isspace():
             self.add_error(
@@ -54,6 +81,12 @@ class StoryForm(forms.ModelForm):
         return title
 
     def clean_excerpt(self):
+        """
+        Validates the 'excerpt' field by:
+        - Stripping HTML tags.
+        - Ensuring the excerpt is not empty.
+        - Limiting the length to 500 characters.
+        """
         excerpt = self.cleaned_data.get('excerpt')
         excerpt = strip_tags(excerpt)
         if not excerpt or excerpt.isspace():
@@ -66,6 +99,11 @@ class StoryForm(forms.ModelForm):
         return excerpt
 
     def clean_content(self):
+        """
+        Validates the 'content' field by:
+        - Stripping HTML tags.
+        - Ensuring the content is not empty.
+        """
         content = self.cleaned_data.get('content')
         content = strip_tags(content)
         if not content or content.isspace():
